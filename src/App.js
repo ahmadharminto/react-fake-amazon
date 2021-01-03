11576ab4937dbbad1696e00008d0ff0e028461c8
Ahmad Harminto <harminto_ahmad@live.com>
2021-01-03 15:14:06 +0700

    add helmet

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import Checkout from './Checkout';
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Payment from "./Payment";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import Page404 from "./Page404";
 import Orders from "./Orders";
+import { Helmet } from "react-helmet";
 
 const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 function App() {
-  document.title = 'Fake Amazon.com'
-
+  const [defaultTitle, setDefaultTitle] = useState('Fake Amazon.com')
   const [{}, dispatch] = useStateValue()
 
   useEffect(() => {
@@ -41,27 +41,45 @@ function App() {
        <div className="app">
          <Switch>
             <Route exact path="/">
+              <Helmet>
+                <title>{defaultTitle}</title>
+              </Helmet>
               <Header />
               <Home />
             </Route>
             <Route path="/login">
+              <Helmet>
+                <title>{`Login | ${defaultTitle}`}</title>
+              </Helmet>
               <Login />
             </Route>
             <Route path="/checkout">
+              <Helmet>
+                <title>{`Checkout | ${defaultTitle}`}</title>
+              </Helmet>
               <Header />
               <Checkout />
             </Route>
             <Route path="/payment">
+              <Helmet>
+                <title>{`Payment | ${defaultTitle}`}</title>
+              </Helmet>
               <Header />
               <Elements stripe={promise}>
                 <Payment />
               </Elements>
             </Route>
             <Route path="/orders">
+              <Helmet>
+                <title>{`Order Hist | ${defaultTitle}`}</title>
+              </Helmet>
               <Header />
               <Orders />
             </Route>
             <Route>
+              <Helmet>
+                <title>{`404 | ${defaultTitle}`}</title>
+              </Helmet>
               <Page404 />
             </Route>
          </Switch>
